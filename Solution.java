7324a37947f829bf79e2ffc67b8faef25d1f3fc7
Aetheress <55312233+Aetheress@users.noreply.github.com>
2019-09-30 21:18:37 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+
+	static boolean palindrome(String s) {
+		s = s.toLowerCase();
+		if (s.length()==0 || s.length()==1) return true;
+		else if (s.charAt(0)==s.charAt(s.length()-1)) {
+			return palindrome(s.substring(1, s.length()-1));
+		}
+		else return false;
+	}
+	
+	static void result(String s) {
+		if (palindrome(s)) {
+			System.out.println("Given string \"" + s + "\" is a palindrome.");
+		}
+		else System.out.println("Given string \"" + s + "\" is NOT a palindrome.");
+	}
+	
+	public static void main(String[] args) {
+		result("abcba");
+		result("argentinamanitnegra");
+		result("Sapalsaritadedatiraslapas");	//s.toLowerCase() ir ieviests tieši šī piemēra dēļ
+		result("abccb");
+		result("stirna");
+		result("mammam");	//not mentioned in examples but I implemented even palindromes as well, not just odd ones
+	}
+}
